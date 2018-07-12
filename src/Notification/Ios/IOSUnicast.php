9010711292d847f8b4be917a55662ef38f0f6129
root <root@Konohanaruto.(none)>
2018-07-12 10:59:14 +0800

    init

---
+++ src/Notification/Ios/IOSUnicast.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * (c) CopyRight <kingshi2010@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Konohanaruto\Notification\Ios;
+
+use Konohanaruto\Notification\IOSNotification;
+
+class IOSUnicast extends IOSNotification {
+	function __construct() {
+		parent::__construct();
+		$this->data["type"] = "unicast";
+		$this->data["device_tokens"] = NULL;
+	}
+
+}
